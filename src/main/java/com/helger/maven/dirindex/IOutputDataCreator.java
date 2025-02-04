4f3348685d05bfc8fc1e303ec8e966dd7733a929
Philip Helger <philip@helger.com>
2025-02-04 18:17:35 +0100

    Added output creation abstraction

---
+++ src/main/java/com/helger/maven/dirindex/IOutputDataCreator.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2014-2025 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.maven.dirindex;
+
+import java.io.File;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+
+import com.helger.commons.annotation.Nonempty;
+import com.helger.commons.state.ESuccess;
+
+/**
+ * Abstract output data creator interface
+ *
+ * @author Philip Helger
+ */
+public interface IOutputDataCreator
+{
+  void init (@Nonnull @Nonempty String sSourceDirectory);
+
+  void addDirectory (@Nonnull @Nonempty String sDirectoryName,
+                     @Nonnull @Nonempty String sBaseName,
+                     @Nonnegative int nSubDirCount,
+                     @Nonnegative int nFileCount);
+
+  void addFile (@Nonnull @Nonempty String sFileName, @Nonnull @Nonempty String sBaseName, @Nonnegative long nFileSize);
+
+  void addFinalSums (@Nonnegative int nTotalDirs, @Nonnegative int nTotalFiles);
+
+  @Nonnull
+  ESuccess writeToFile (@Nonnull File aTarget);
+}
